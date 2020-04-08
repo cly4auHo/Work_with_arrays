614d2b18da38ffe62c5fd7ceabf1a18ab284d2b1
Arsen <52761327+cly4auHo@users.noreply.github.com>
2020-04-08 16:25:22 +0300

    Update FoundMin.java

--- FoundMin.java
+++ FoundMin.java
@@ -2,18 +2,27 @@ package arraywork;
 
 public class FoundMin {
 
-    public static void main(String[] args) {
-        int[] array = {123, 11, 9, 7, 0, 123, 44};
-        int minValues = array[0];
-        int minIndex = 0;
+ public static void main(String[] args) {
+        int [] array = new int[] {64, 42, 73, 41, 32, 53, 16, 24, 57, 42, 74, 55, 36};
 
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] < minValues) {
-                minValues = array[i];
-                minIndex = i;
+        int min = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min2 = min;
+                min = array[i];
+            } else if (array[i] < min2 && array[i] != min) {
+                min2 = array[i];
             }
         }
-        System.out.println(minValues);
-        System.out.println(minIndex);
+
+        if (min2 != Integer.MAX_VALUE) {
+            System.out.println("Второй по минимальности элемент в массиве: " + min2);
+        } else {
+            System.out.println("Второго по минимальности элемента нет");
+        }
+
+        System.out.println("Минимальный элемент в массиве: " + min);
     }
 }
